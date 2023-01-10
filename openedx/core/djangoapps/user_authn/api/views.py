65dc187328aa75f38c91908196c704dbb6a0ea22
Syed Sajjad Hussain Shah <syed.sajjad@arbisoft.com>
2023-01-10 14:50:44 +0500

    fix: update MFE context response
    VAN-1231

--- openedx/core/djangoapps/user_authn/api/views.py
+++ openedx/core/djangoapps/user_authn/api/views.py
@@ -58,19 +58,17 @@ class MFEContextView(APIView):
             'optional_fields': {},
         }
 
-        if settings.ENABLE_DYNAMIC_REGISTRATION_FIELDS:
-            if is_register_page:
-                registration_fields = RegistrationFieldsContext().get_fields()
-                context['registration_fields'].update({
-                    'fields': registration_fields,
+        if settings.ENABLE_DYNAMIC_REGISTRATION_FIELDS and is_register_page:
+            registration_fields = RegistrationFieldsContext().get_fields()
+            context['registration_fields'].update({
+                'fields': registration_fields,
+            })
+            optional_fields = RegistrationFieldsContext('optional').get_fields()
+            if optional_fields:
+                context['optional_fields'].update({
+                    'fields': optional_fields,
                     'extended_profile': configuration_helpers.get_value('extended_profile_fields', []),
                 })
-                optional_fields = RegistrationFieldsContext('optional').get_fields()
-                if optional_fields:
-                    context['optional_fields'].update({
-                        'fields': optional_fields,
-                        'extended_profile': configuration_helpers.get_value('extended_profile_fields', []),
-                    })
 
         return Response(
             status=status.HTTP_200_OK,
